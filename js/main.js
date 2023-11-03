fe2331765241236b1aeb05960c294f84d0716b89
Erdal Tok <erdaltok@hotmail.de>
2023-11-04 00:38:21 +0100

    next drag and drop board

--- js/main.js
+++ js/main.js
@@ -38,3 +38,85 @@ function showContacts() {
   displayContentTemplates("contacts");
 }
 
+
+
+// let tasks = [
+//   {
+//     id: 0,
+//     title: "Task 1",
+//     category: "todo",
+//   },
+//   {
+//     id: 1,
+//     title: "Task 2",
+//     category: "inProgress",
+//   },
+//   {
+//     id: 2,
+//     title: "Task 3",
+//     category: "awaitFeedback",
+//   },
+//   {
+//     id: 3,
+//     title: "Task 4",
+//     category: "done",
+//   },
+// ];
+
+// let currentDraggedElement;
+
+// function updateHTML() {
+//   const categories = ["todo", "inProgress", "awaitFeedback", "done"];
+//   categories.forEach((category) => {
+//     let filteredTasks = tasks.filter((task) => task.category === category);
+//     document.getElementById(category).innerHTML = "";
+//     filteredTasks.forEach((task) => {
+//       document.getElementById(category).innerHTML += generateTaskHTML(task);
+//     });
+//   });
+// }
+
+// function startDragging(id) {
+//   // Finden Sie die Aufgabe, die verschoben wird, und setzen Sie currentDraggedElement auf das Task-Objekt
+//   currentDraggedElement = tasks.find((task) => task.id === id);
+// }
+
+// function generateTaskHTML(task) {
+//   // Verwenden Sie die Aufgaben-ID, um die startDragging-Funktion aufzurufen
+//   return `<div draggable="true" ondragstart="startDragging(${task.id})" class="task">${task.title}</div>`;
+// }
+
+
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
+
+// function moveTo(category, ev) {
+//   ev.preventDefault();
+//   if (currentDraggedElement) {
+//     // Setzen Sie die Kategorie der aktuellen gezogenen Aufgabe
+//     currentDraggedElement.category = category;
+//     updateHTML();
+//   } else {
+//     console.error("Kein gültiges gezogenes Element gefunden");
+//   }
+// }
+
+// function highlight(id) {
+//   document.getElementById(id).classList.add("highlight");
+// }
+
+// function removeHighlight(id) {
+//   document.getElementById(id).classList.remove("highlight");
+// }
+
+// // Initial update on page load
+// document.addEventListener("DOMContentLoaded", updateHTML);
+
+
+
+
+
+
+
+
