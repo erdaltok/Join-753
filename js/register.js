84e5bf331ed02adcd43086f1d538bffde8cafa45
DenVSHard <denisweinhardt@icloud.com>
2023-11-06 00:24:29 +0100

    W

--- js/register.js
+++ js/register.js
@@ -1,5 +1,14 @@
-    window.onload = function () {
+    function init() {
         let logo = document.getElementById('logo');
         document.body.classList.add('animate');
+        show();
     }
 
+    function show() {
+        setTimeout(function () {
+            let element = document.getElementById("animation","sing-up");
+            if (element) {
+                element.style.display = "flex";
+            }
+        }, 1000);
+    }
