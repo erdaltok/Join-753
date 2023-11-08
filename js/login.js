3a3578a9eb1b11c660f725f89f069426d306ff1e
DenVSHard <denisweinhardt@icloud.com>
2023-11-08 23:09:04 +0100

    W

--- js/login.js
+++ js/login.js
@@ -1,19 +1,37 @@
-    function init() {
-        let logo = document.getElementById('logo');
-        document.body.classList.add('animate');
-        
-        show();
-    }
+function init() {
+    let logo = document.getElementById('logo');
+    document.body.classList.add('animate');
 
-    function show() {
-        setTimeout(function () {
-            let element = document.getElementById("animation");
-            if (element) {
-                element.style.display = "flex";
-            }
-        }, 1000);
-    }
+    show();
+}
 
-    function guestLogin() {
-        window.location.href = "index.html";
+function show() {
+    setTimeout(function () {
+        let element = document.getElementById("animation");
+        if (element) {
+            element.style.display = "flex";
+        }
+    }, 1000);
+}
+
+function guestLogin() {
+    window.location.href = "index.html";
+}
+
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let users = users.find(u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if(user) {
+        console.log('user gefunden')
     }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+if (msg) {
+    msg_box.innerHTML = msg;
+} else {
+    //display:none;
+}
--- js/register.js
+++ js/register.js
@@ -1,3 +1,12 @@
 let users = [
-    {'email':'denis@test.de', 'password':'test123'}
+    {'name':'Denis', 'email':'denis@test.de', 'password':'test123'}
 ];
+
+function addUser() {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    users.push({name:name.value, email:email.value, password:password.value});
+
+    window.location.href = 'login.html?msg=You Signed Up successfully';
+}
