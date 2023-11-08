abe11cfaf12aaded789537758fdfce24d32883fa
Daniel Toma <vamix64@gmail.com>
2023-11-08 18:19:10 +0100

    addTask Update

--- js/addTask.js
+++ js/addTask.js
@@ -1,7 +1,24 @@
-const btn = document.querySelector('.prioButtonsBox button');
 
-btn.addEventListener('click', () => {
-        btn.style.backgroundColor = 'green';
-        
-    
-});
+
+
+function colorChange1() {
+        const button = document.querySelector('#btn1');
+        const currentColor = button.style.backgroundColor;
+      
+        if (currentColor === 'rgb(51, 51, 51)' || currentColor === 'red') {
+          button.style.backgroundColor = 'white';
+        } else {
+          button.style.backgroundColor = 'red';
+        }
+      }
+
+      function colorChange2() {
+        const button = document.querySelector('#btn2');
+        const currentColor = button.style.backgroundColor;
+      
+        if (currentColor === 'rgb(51, 51, 51)' || currentColor === 'yellow') {
+          button.style.backgroundColor = 'white';
+        } else {
+          button.style.backgroundColor = 'yellow';
+        }
+      }
