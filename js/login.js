0c520a120260c73cbbd1fe187a0072d550bec82e
DenVSHard <denisweinhardt@icloud.com>
2023-11-10 00:20:27 +0100

    W

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function load() {
     let logo = document.getElementById('logo');
     document.body.classList.add('animate');
-
+    loadUsers();
     show();
 }
 
@@ -21,9 +21,9 @@ function guestLogin() {
 function login() {
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    let users = users.find(u => u.email == email.value && u.password == password.value);
-    console.log(user);
-    if(user) {
+    let users = users.find(u => u.name == name.value, u.email == email.value && u.password == password.value);
+    console.log(users);
+    if (users) {
         console.log('user gefunden')
     }
 }
--- js/register.js
+++ js/register.js
@@ -1,31 +1,43 @@
 let users = [];
 
-
-async function init(){
+async function init() {
     loadUsers();
 }
 
-async function loadUsers(){
+async function loadUsers() {
     try {
         users = JSON.parse(await getItem('users'));
-    } catch(e){
+    } catch (e) {
         console.error('Loading error:', e);
     }
 }
 
-
 async function register() {
-    registerBtn.disabled = true;
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
+
+    if (password.value !== confirmPassword.value) {
+        alert('Passwörter stimmen nicht überein');
+        return;
+    }
+
+    document.getElementById('register_btn').disabled = true;
     users.push({
+        name: name.value,
         email: email.value,
         password: password.value,
     });
     await setItem('users', JSON.stringify(users));
     resetForm();
+    window.location.href = 'login.html?msg=Registrierung ervolgreich';
 }
 
 function resetForm() {
+    name = '';
     email.value = '';
     password.value = '';
-    registerBtn.disabled = false;
+    confirmPassword = '';
+    document.getElementById('register_btn').disabled = false;
 }
