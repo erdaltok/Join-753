94805d522731d72a2657a6040843f4bb42798198
DenVSHard <denisweinhardt@icloud.com>
2023-11-20 21:54:56 +0100

    W

--- js/contacts.js
+++ js/contacts.js
@@ -1,45 +1,83 @@
+let names = [];
+let emails = [];
+let phones = [];
+
+
 function changeColor() {
-  let contactDiv = document.getElementById("contact");
-  contactDiv.classList.add("clicked");
+    let contactDiv = document.getElementById("contact");
+    contactDiv.classList.add("clicked");
 }
 
-function showCard() {
-  document.getElementById('card').innerHTML = ``;
-  document.getElementById('card').innerHTML = `
-    <div class="card">
-      <div>
-        <p class="initial-2">BS</p>
-      </div>
+function showCard(name, email, phone) {
+    let initials = getInitials(name);
+    document.getElementById('card').innerHTML = ``;
+    document.getElementById('card').innerHTML = `
+      <div class="card">
+        <div>
+        <p class="initial-2">${initials}</p>
+        </div>
         <div style="line-height: 0.1;">
-           <h3 class="name">Alex Mustermann</h3>
+          <h3 class="name">${name}</h3>
           <div class="del">
             <div class="edit-delete">
-                <img style="cursor: default;" class="edit-del" src="/img/edit.png">
-                <p onclick="editContact()" class="edit-del">Edit</p>
+              <img style="cursor: default;" class="edit-del" src="/img/edit.png">
+              <p onclick="editContact()" class="edit-del">Edit</p>
             </div>
             <div class="edit-delete">
-                <img style="cursor: default;" class="edit-del" src="/img/delete.png">
-                <p class="edit-del">Delete</p>
+              <img style="cursor: default;" class="edit-del" src="/img/delete.png">
+              <p class="edit-del">Delete</p>
             </div>
           </div>
         </div>
-    </div>
-    <div class="information">
-     <p>Contact Information</p>
-    </div>
-    <div class="info">
-     <p class="info-1">Email</p>
-     <a class="info-2" href="mailto:alex@gmail.com">alex@gmail.com</a>
-     <p class="info-1">Phone</p>
-     <a class="info-2" href="tel:01511234567">0151/1234567</a>
-    </div>
+      </div>
+      <div class="information">
+        <p>Kontaktinformationen</p>
+      </div>
+      <div class="info">
+        <p class="info-1">E-Mail</p>
+        <a class="info-2" href="mailto:${email}">${email}</a>
+        <p class="info-1">Telefon</p>
+        <a class="info-2" href="tel:${phone}">${phone}</a>
+      </div>
     `;
 }
 
+function advanceCard(name, email) {
+    let initials = getInitials(name);
+  
+    document.getElementById('advanceCard').innerHTML = `
+      <div>
+        <div>
+          <p class="initial-letter">${initials}</p>
+          <div class="line"></div>
+        </div>
+        <div id="contact" onclick="changeColor(); showCard('${name}', '${email}');" class="contact">
+          <div>
+            <p class="initial">${initials}</p>
+          </div>
+          <div class="initial-text">
+            <p>${name}</p>
+            <p>${email}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+  
+
+function getInitials(name) {
+    let splitName = name.split(' ');
+    let initials = '';
+    for (let i = 0; i < splitName.length; i++) {
+        initials += splitName[i][0].toUpperCase();
+    }
+    return initials;
+}
+
 function newContact() {
-  let openDiv = document.getElementById('newContact');
-  openDiv.style.display = 'flex';
-  openDiv.innerHTML = `
+    let openDiv = document.getElementById('newContact');
+    openDiv.style.display = 'flex';
+    openDiv.innerHTML = `
     <div class="div-container">
         <div class="new-card">
             <img src="/img/icon2.png">
@@ -59,7 +97,8 @@ function newContact() {
             <div class="close">
                 <img onclick="closeNewContact()" src="/img/close.png">
             </div>
-            <div class="div-input-fields">
+            <form onsubmit="createContact(event)">
+             <div class="div-input-fields">
                 <div class="input-fields">
                     <input class="input" required type="text" placeholder="Name">
                     <img src="/img/person.png" alt="icon" style="width: 24; height: 24;">
@@ -76,21 +115,41 @@ function newContact() {
                     <button onclick="closeNewContact()" class="cancel-btn">Cancel <img src="/img/cencel.png" alt=""></button>
                     <button style="position: relative; right: 40px;" class="create-btn">Create contact <img src="/img/check.png" alt=""></button>
                 </div>
-            </div>
+             </div>
+            </form>
         </div>
     </div>
   `;
 }
 
 function closeNewContact() {
-  let openDiv = document.getElementById('newContact');
-  openDiv.style.display = 'none';
+    let openDiv = document.getElementById('newContact');
+    openDiv.style.display = 'none';
+}
+
+function createContact(event) {
+    event.preventDefault();
+    let nameInput = document.querySelector('.input-fields input[placeholder="Name"]');
+    let emailInput = document.querySelector('.input-fields input[placeholder="Email"]');
+    let phoneInput = document.querySelector('.input-fields input[placeholder="Phone"]');
+
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let phone = phoneInput.value;
+
+    names.push(name);
+    emails.push(email);
+    phones.push(phone);
+
+    showCard(name, email, phone);
+    advanceCard(name,email);
+    closeNewContact();
 }
 
 function editContact() {
-  let openDiv = document.getElementById('editContact');
-  openDiv.style.display = 'flex';
-  openDiv.innerHTML = `
+    let openDiv = document.getElementById('editContact');
+    openDiv.style.display = 'flex';
+    openDiv.innerHTML = `
         <div class="div-container">
             <div class="new-card">
                 <img src="/img/icon2.png">
@@ -135,6 +194,6 @@ function editContact() {
 }
 
 function closeEditContact() {
-  let openDiv = document.getElementById('editContact');
-  openDiv.style.display = 'none';
+    let openDiv = document.getElementById('editContact');
+    openDiv.style.display = 'none';
 }
