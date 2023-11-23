da60b7a022b8ee64b872c910d16bd870ec2cf2a6
DenVSHard <denisweinhardt@icloud.com>
2023-11-23 23:50:57 +0100

    W

--- js/contacts.js
+++ js/contacts.js
@@ -251,53 +251,96 @@ function updateContactDisplay() {
 
 
 function editContact() {
-    let openDiv = document.getElementById('editContact');
-    openDiv.style.display = 'flex';
-    openDiv.innerHTML = `
-        <div class="div-container">
-            <div class="new-card">
-                <img src="/img/icon2.png">
-                <h1 class="new-text">Edit contact</h1>
-                <p class="new-text-2">Tasks are better with a team</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="88" height="3" viewBox="0 0 3 3" fill="none"
-                    style="width: 88px; height: 3px;">
-                    <path d="M2 2V61" stroke="#29ABE2" stroke-width="88" stroke-linecap="round" />
-                </svg>
+    let selectedContact = document.querySelector('.contact.clicked');
+
+    if (selectedContact) {
+        let index = selectedContact.dataset.index;
+
+        let openDiv = document.getElementById('editContact');
+        openDiv.style.display = 'flex';
+        openDiv.innerHTML = `
+            <div class="div-container">
+                <div class="new-card">
+                    <img src="/img/icon2.png">
+                    <h1 class="new-text">Edit contact</h1>
+                    <p class="new-text-2">Tasks are better with a team</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="88" height="3" viewBox="0 0 3 3" fill="none"
+                        style="width: 88px; height: 3px;">
+                        <path d="M2 2V61" stroke="#29ABE2" stroke-width="88" stroke-linecap="round" />
+                    </svg>
+                </div>
+                <div class=" div-input">
+                    <div class="picture-div">
+                        <div class="picture">
+                            <img style="width: 42px; height: 42px;" src="/img/person (1).png">
+                        </div>
+                    </div>
+                    <div class="close">
+                        <img onclick="closeEditContact()" src="/img/close.png">
+                    </div>
+                    <form onsubmit="saveEditedContact(event, ${index})">
+                        <div class="div-input-fields">
+                            <div class="input-fields">
+                                <input class="input" required type="text" placeholder="Name" value="${names[index]}">
+                                <img src="/img/person.png" alt="icon" style="width: 24; height: 24;">
+                            </div>
+                            <div class="input-fields">
+                                <input class="input" required type="email" placeholder="Email" value="${emails[index]}">
+                                <img src="/img/mail.png" alt="icon" class="icon-1">
+                            </div>
+                            <div class="input-fields">
+                                <input class="input" required type="tel" placeholder="Phone" value="${phones[index]}">
+                                <img src="/img/call.png" alt="icon" class="icon-2">
+                            </div>
+                            <div class="buttons">
+                                <button class="cancel-btn" onclick="deleteEditedContact(${index})">Delete <img src="/img/cencel.png" alt=""></button>
+                                <button style="position: relative; right: 40px;" class="create-btn" type="submit">Save <img src="/img/check.png" alt=""></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class=" div-input">
-                <div class="picture-div">
-                    <div class="picture">
-                        <img style="width: 42px; height: 42px;" src="/img/person (1).png">
-                    </div>
-                </div>
-                <div class="close">
-                    <img onclick="closeEditContact()" src="/img/close.png">
-                </div>
-                <div class="div-input-fields">
-                    <div class="input-fields">
-                        <input class="input" required type="text" placeholder="Name">
-                        <img src="/img/person.png" alt="icon" style="width: 24; height: 24;">
-                    </div>
-                    <div class="input-fields">
-                        <input class="input" required type="email" placeholder="Email">
-                        <img src="/img/mail.png" alt="icon" class="icon-1">
-                    </div>
-                    <div class="input-fields">
-                        <input class="input" required type="phone" placeholder="Phone">
-                        <img src="/img/call.png" alt="icon" class="icon-2">
-                    </div>
-                    <div class="buttons">
-                        <button class="cancel-btn">Delete <img src="/img/cencel.png" alt=""></button>
-                        <button style="position: relative; right: 40px;" class="create-btn">Save <img
-                                src="/img/check.png" alt=""></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-  `;
+        `;
+    }
+}
+
+
+function saveEditedContact(event, index) {
+    event.preventDefault();
+    let nameInput = document.querySelector('.input-fields input[placeholder="Name"]');
+    let emailInput = document.querySelector('.input-fields input[placeholder="Email"]');
+    let phoneInput = document.querySelector('.input-fields input[placeholder="Phone"]');
+
+    let newName = nameInput.value;
+    let newEmail = emailInput.value;
+    let newPhone = phoneInput.value;
+
+    names[index] = newName;
+    emails[index] = newEmail;
+    phones[index] = newPhone;
 
     saveContactsToLocalStorage();
+    showCard(newName, newEmail, newPhone, index);
+    updateContactDisplay();
+    closeEditContact();
 }
+
+
+function deleteEditedContact(index) {
+    let selectedContact = document.querySelector('.contact.clicked');
+
+    if (selectedContact) {
+
+        names.splice(index, 1);
+        emails.splice(index, 1);
+        phones.splice(index, 1);
+
+        saveContactsToLocalStorage();
+        updateContactDisplay();
+        closeEditContact();
+    }
+}
+
 
 
 function closeEditContact() {
